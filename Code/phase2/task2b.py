8d8ac61ad6d0d652e804804affb48c38e2327d3d
rahul_rajaram <rajaramrahul2@gmail.com>
2023-10-09 08:55:38 -0700

    task 2b completion

--- Code/phase2/task2b.py
+++ Code/phase2/task2b.py
@@ -4,21 +4,34 @@ import numpy as np
 import torch
 import numpy as np
 from PIL import Image
+import torchvision.transforms as T
+from torch.nn import functional as F
+import torch.nn as nn
 import sys
-sys.path.append('C:\Khadyu\ASU\Fall 2023\Multimedia & Web Databases\Project\Phase2\cse515-project\Code')
-from extracting_feature_space import color_moments
-import extracting_feature_space.HOG as HOG
-import extracting_feature_space.resnet_features as resnet_features
-
-def task2b(query_image_id, query_image_file, k):
+from torchvision.models import resnet50
+basePath='D:\\Project Multimedia\\new_phase2_repo\\cse515project'
+def task2b(query_image_id, query_image_file, k,category_names):
+    data_transforms = T.Compose([T.Resize((224,224)), T.ToTensor(),T.Lambda(lambda x: x.repeat(3, 1, 1) if x.shape[0]==1 else x)])
     if query_image_id != None:
         for image_id, (image, label) in enumerate(dataset):
             if image_id == int(query_image_id):
                 query_image_data = image
+                print(f'label:{label}')
                 break
     elif query_image_file != None:
-        query_image_data = Image.open(query_image_file)
-        
+        query_image_data = data_transforms(Image.open(query_image_file))
+    resize = T.Lambda(lambda x: x.repeat(3, 1, 1) if x.size(0)==1 else x)
+   
+    myTrainedmodel = resnet50(pretrained=False)
+    myTrainedmodel.fc = nn.Linear(2048, 101)
+    myTrainedmodel.load_state_dict(torch.load(basePath+'\\Code\\TrainedModels pkls\\resnetMod101.pkl' ,map_location=torch.device('cpu')))
+    preds_tensor = myTrainedmodel(query_image_data.reshape(-1,3,224,224))
+    pred_probs = F.softmax(preds_tensor, dim=1).to("cpu").data.numpy()
+    probabilties=[ (idx,ele) for idx,ele in enumerate(pred_probs[0])]
+    probabilties=sorted(probabilties,key= lambda a: a[1],reverse=True)
+    for i in range(k):
+        print(f'k:{str(i+1).ljust(2," ")} category name: {category_names[int(probabilties[i][0])].ljust(25," ")} label no : {str(int(probabilties[i][0])).ljust(3," ")} probability: {probabilties[i][1]}')
+
     
 
 if __name__ == "__main__":
@@ -26,15 +39,17 @@ if __name__ == "__main__":
     db = cl["caltech101db"]
     collection = db["phase2trainingdataset"]
     collection_name = "phase2trainingdataset"
-
-    caltech101_directory = "C:/Khadyu/ASU/Fall 2023/Multimedia & Web Databases/Project/Phase1/data"
-    dataset = datasets.Caltech101(caltech101_directory, download=False)
+    caltech101_directory='./data' 
+    resize = T.Lambda(lambda x: x.repeat(3, 1, 1) if x.size(0)==1 else x)
+    data_transforms = T.Compose([T.Resize((224,224)), T.ToTensor(),T.Lambda(lambda x: x.repeat(3, 1, 1) if x.shape[0]==1 else x)])
+    dataset = datasets.Caltech101(caltech101_directory,transform = data_transforms ,download=True)
+    category_names = dataset.annotation_categories 
     data_loader = torch.utils.data.DataLoader(dataset, batch_size=4, shuffle=True, num_workers=8)
-
     print("Enter '1' if you want to give an image ID as input or enter '2' if you want to give Image File as an Input: ")
     query_type = int(input("Enter query type: "))
     query_image_id = None
     query_image_file = None
+
     if query_type == 1:
         query_image_id = int(input("Enter query image ID: "))
     elif query_type == 2:
@@ -42,6 +57,5 @@ if __name__ == "__main__":
     else: 
         print("Enter valid query type!")
 
-    k = input("Enter k: ")
-    
-    task2b(query_image_id, query_image_file, k)
+    k = int(input("Enter k: "))
+    task2b(query_image_id, query_image_file, k,category_names)
