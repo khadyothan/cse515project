c6d4e8b47589333c1e3801f27fab998a7e422130
Lalitarvind <lalitarvind.balaji@gmail.com>
2023-10-11 14:38:41 -0700

    LDA Code

--- Code/data_matrix/dataMatrix.py
+++ Code/data_matrix/dataMatrix.py
@@ -5,10 +5,10 @@ import torchvision.datasets as datasets
 
 cl = pymongo.MongoClient("mongodb://localhost:27017")
 db = cl["caltech101db"]
-collection = db["phase2trainingdataset"]
+collection = db["caltech101withGSimages"]
 
-caltech101_directory = "C:/Khadyu/ASU/Fall 2023/Multimedia & Web Databases/Project/Phase1/data"
-dataset = datasets.Caltech101(caltech101_directory, download=False)
+caltech101_directory = "/Users/lalitarvind/Downloads/MWD_Team_project_v1/"
+dataset = datasets.Caltech101(caltech101_directory, download=True)
 
 def createMatrix(feature_descriptor):
     data_vector = collection.find_one({"image_id": 0, })[feature_descriptor]
@@ -29,14 +29,14 @@ data_matrix_hog = createMatrix("hog_feature_descriptor")
 data_matrix_layer3 = createMatrix("resnet50_layer3_feature_descriptor")
 data_matrix_avgpool = createMatrix("resnet50_avgpool_feature_descriptor")
 data_matrix_fc = createMatrix("resnet50_fc_feature_descriptor")
-
-file_path_cm = "C:\Khadyu\ASU\Fall 2023\Multimedia & Web Databases\Project\Phase2\cse515-project\Code\dimensionality_reduction\data_matrix_cm.csv"
+basepath = "/Users/lalitarvind/Downloads/MWD_Team_project_v1/cse515project/Code/dimensionality_reduction/"
+file_path_cm = basepath + "data_matrix_cm.csv"
 np.savetxt(file_path_cm, data_matrix_cm, delimiter=",")
-file_path_hog = "C:\Khadyu\ASU\Fall 2023\Multimedia & Web Databases\Project\Phase2\cse515-project\Code\dimensionality_reduction\data_matrix_hog.csv"
+file_path_hog = basepath + "data_matrix_hog.csv"
 np.savetxt(file_path_hog, data_matrix_hog, delimiter=",")
-file_path_layer3 = "C:\Khadyu\ASU\Fall 2023\Multimedia & Web Databases\Project\Phase2\cse515-project\Code\dimensionality_reduction\data_matrix_layer3.csv"
+file_path_layer3 = basepath + "data_matrix_layer3.csv"
 np.savetxt(file_path_layer3, data_matrix_layer3, delimiter=",")
-file_path_avgpool = "C:\Khadyu\ASU\Fall 2023\Multimedia & Web Databases\Project\Phase2\cse515-project\Code\dimensionality_reduction\data_matrix_avgpool.csv"
+file_path_avgpool = basepath + "data_matrix_avgpool.csv"
 np.savetxt(file_path_avgpool, data_matrix_avgpool, delimiter=",")
-file_path_fc = "C:\Khadyu\ASU\Fall 2023\Multimedia & Web Databases\Project\Phase2\cse515-project\Code\dimensionality_reduction\data_matrix_fc.csv"
+file_path_fc = basepath + "data_matrix_fc.csv"
 np.savetxt(file_path_fc, data_matrix_fc, delimiter=",")
--- Code/dimensionality_reduction/LDA/lda.py
+++ Code/dimensionality_reduction/LDA/lda.py
@@ -1,18 +1,58 @@
 import numpy as np
-
-
-def lda(data_matrix, k):
-    print(data_matrix.shape)
+import json
+from sklearn.decomposition import LatentDirichletAllocation
+import csv
+def lda(data_matrix,feature_descriptor,k):
+    data_matrix = np.array(data_matrix)
+    data_matrix_min_val = data_matrix.min()
+    if data_matrix_min_val<0:
+        data_matrix = data_matrix - data_matrix_min_val
+    fileExist=False
+    ls_path = "/Users/lalitarvind/Downloads/MWD_Team_project_v1/cse515project/Code/latent_semantics/"
+    try:
+        f = open(f"{ls_path}lda_{feature_descriptor}_{k}_ls.json","r")
+        fileExist=True
+    except:
+        fileExist=False
     
+    if(not fileExist):
+        print("Creating file...")
+        lda = LatentDirichletAllocation(n_components=k,verbose=True)
+        lda.fit(data_matrix)
+        LDA_factor_matrix = lda.components_ /lda.components_.sum(axis=1)[:, np.newaxis]
+        f1 = open(f"{ls_path}lda_{feature_descriptor}_{k}_ls.json","w")
+        json.dump(LDA_factor_matrix.tolist() ,f1)
+        f1.close()
+
+    f = open(f"{ls_path}lda_{feature_descriptor}_{k}_ls.json","r")
+    ls = json.loads(f.read())
+    ls = np.transpose(ls)
+    #query = np.array(data_matrix).reshape(len(data_matrix),len(data_matrix[0]))
+    query_distribution = np.matmul(data_matrix,ls)
+    f.close()
+    return query_distribution
+
+def calculateImageIDWeightPairs(feature_descriptor_ls,feature_descriptor,k): 
+    image_id_weight_pairs = []
+    ls_matrix = feature_descriptor_ls
+    for i in range(len(ls_matrix)):
+        image_id = i*2
+        weight = np.linalg.norm(ls_matrix[i, :])
+        image_id_weight_pairs.append((image_id, weight))
     
-    data_matrix_ls = {}
-    return data_matrix_ls
+    sorted_image_id_weight_pairs_cm = sorted(image_id_weight_pairs, key=lambda x: x[1], reverse=True) 
+    im_id_weight_basepath = "/Users/lalitarvind/Downloads/MWD_Team_project_v1/cse515project/Code/image_id_weight_pairs/lda/"
+    with open(f"{im_id_weight_basepath}{feature_descriptor}_{k}_ls.json", 'w') as csvfile:  
+        csvwriter = csv.writer(csvfile)  
+        fields = ["Image ID", "Weight"]
+        csvwriter.writerow(fields)
+        for image_id, weight in sorted_image_id_weight_pairs_cm:
+            csvwriter.writerow([image_id,weight])
 
-
-def calculateImageIDWeightPairs(feature_descriptor_ls, feature_descriptor): 
-    
-    return True
-
-if __name__ == "main":
-    data_matrix = np.loadtxt("Code\dimensionality_reduction\data_matrix_cm.csv", delimiter=',')     
-    cm_ls = lda(data_matrix)
+data_matrix_path = "/Users/lalitarvind/Downloads/MWD_Team_project_v1/cse515project/Code/data_matrix/"
+data_matrix = np.loadtxt(f"{data_matrix_path}data_matrix_hog.csv", delimiter=',')  
+#print(len(data_matrix))   
+cm_ls = lda(data_matrix,"hog_feature_descriptor",10)
+print(cm_ls)
+#print(len(cm_ls),len(cm_ls[0]),)
+calculateImageIDWeightPairs(cm_ls,"hog_feature_descriptor",10)
